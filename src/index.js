0f1e94716484f366302cfd768a08379f9d76f796
Prajwal <prajwal.pw00@gmailcom>
2025-02-16 19:27:03 +0530

    Utils setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import dotenv from 'dotenv';
 import connectdb from './db/index.js';
+import app from './app.js';
 
-dotenv.config({path:'./env'});
+dotenv.config({path:'./.env'});
 
 connectdb()
-
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+})
 
 
 
